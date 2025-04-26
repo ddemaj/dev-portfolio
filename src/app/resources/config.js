36ca0db5d4f9f627665eabf201a8e5c8f44e9dcf
Dervish <103032153+ddemaj@users.noreply.github.com>
2025-04-26 16:18:49 -0400

    Update config.js

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -1,12 +1,12 @@
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL = "https://demo.magic-portfolio.com";
+const baseURL = "https://demaj.dev";
 
 const routes = {
   "/": true,
-  "/about": true,
-  "/work": true,
-  "/blog": true,
-  "/gallery": true,
+  "/about": false,
+  "/work": false,
+  "/blog": false,
+  "/gallery": false,
 };
 
 // Enable password protection on selected routes
